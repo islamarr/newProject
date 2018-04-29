4fee29196d59c22414bcac106a67399f3b9e8380
islamarr <38818550+islamarr@users.noreply.github.com>
2018-04-29 11:27:44 +0200

    فف

--- app/src/main/java/com/approcks/myapplication/MainActivity.java
+++ app/src/main/java/com/approcks/myapplication/MainActivity.java
@@ -2,9 +2,11 @@ package com.approcks.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView vv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
